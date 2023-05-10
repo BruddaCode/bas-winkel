49f2d4bb19ae1dea54fd36d6c14567a380af4997
MVDW <maxvdwolf@gmail.com>
2023-05-10 10:12:23 +0200

    Update config.php

--- backend/config.php
+++ backend/config.php
@@ -4,6 +4,6 @@ $db_user = "production";
 $db_pass = "";
 $db_name = "";
 
-$meid_secret_key = "Not yet made :)";
-$meid_client_id = "0";
+$meid_secret_key = "kadhaspowiupqiohefjkdsvm.bzv.mzdojpawpkeioheuigsjbk";
+$meid_client_id = "6326723623576324723";
 ?>
