09580032d511ebff50304c40dc5fdc7a8cf5ea04
BruddaCode <Fabio.wolthuis@gmail.com>
2023-05-16 12:12:24 +0200

    config

--- backend/config.php
+++ backend/config.php
@@ -4,12 +4,6 @@ $db_user = "root";
 $db_pass = "";
 $db_name = "";
 
-<<<<<<< Updated upstream
 $meid_secret_key = "kadhaspowiupqiohefjkdsvm.bzv.mzdojpawpkeioheuigsjbk";
 $meid_client_id = "6326723623576324723";
 ?>
-=======
-$meid_secret_key = "Not yet made :)";
-$meid_client_id = "0";
-?>
->>>>>>> Stashed changes
