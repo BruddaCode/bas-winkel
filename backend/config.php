421a1088c13cf5ff6a32f2e5d8a55c27271cfd38
BruddaCode <Fabio.wolthuis@gmail.com>
2023-05-16 12:08:59 +0200

    assets, levenancier.php

--- backend/config.php
+++ backend/config.php
@@ -1,9 +1,15 @@
 <?php
 $db_host = "localhost";
-$db_user = "production";
+$db_user = "root";
 $db_pass = "";
 $db_name = "";
 
+<<<<<<< Updated upstream
 $meid_secret_key = "kadhaspowiupqiohefjkdsvm.bzv.mzdojpawpkeioheuigsjbk";
 $meid_client_id = "6326723623576324723";
 ?>
+=======
+$meid_secret_key = "Not yet made :)";
+$meid_client_id = "0";
+?>
+>>>>>>> Stashed changes
---
+++ backend/leverancier.php
@@ -0,0 +1,5 @@
+<?php 
+
+    class leverancier {
+        public $leverancierid;
+    }
