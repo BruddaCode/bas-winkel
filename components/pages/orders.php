9d297b8b383d6d66f313bf4158bb633a5a0ebfbb
MVDW <maxvdwolf@gmail.com>
2023-06-02 16:40:19 +0200

    order list working

--- components/pages/orders.php
+++ components/pages/orders.php
@@ -10,7 +10,7 @@ include_once("components/elements/table.php");
 $table = new elTable();
 $order = new order();
 $table_head = ["Order NR", "Artikel NR", "Klant NR", "Datum", "Aantal", "Status"];
-$table_body = 
+$table_body = iterator_to_array($order->selectOrders());
 
 $table->generateTable($table_head, $table_body);
 
