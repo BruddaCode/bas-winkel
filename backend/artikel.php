1030a22fb633620b504be6b20cea24e827b60239
MVDW <maxvdwolf@gmail.com>
2023-06-04 17:35:01 +0200

    artikel listing

--- backend/artikel.php
+++ backend/artikel.php
@@ -1,11 +1,33 @@
-<?php 
+<?php
+include_once 'crud.php';
+class artikel extends crud
+{
 
-    class artikel {
-        public $artikelid;
-        public $naam;
-        public $prijs;
-        public $omschrijving;
-        public $categorie;
+	public function __construct()
+	{
+		parent::__construct();
+	}
 
-        
-    }
+	public function insertArtikel($omschrijving, $inkoop, $verkoop, $voorraad, $minvoorraad, $maxvoorraad, $locatie, $levid)
+	{
+		return $this->insert("artikelen", array("artikelenomschrijving" => $omschrijving, "artinkoop" => $inkoop, "artverkoop" => $verkoop, "artvoorraad" => $voorraad, "artminvoorraad" => $minvoorraad, "artmaxvoorraad" => $maxvoorraad, "artlocatie" => $locatie, "levid" => $levid));
+	}
+	public function selectArtikel($id = false)
+	{
+		if ($id) {
+			return $this->select("artikelen", "*", "artid=" . $id);
+		} else {
+			return $this->select("artikelen", "*");
+		}
+
+	}
+	public function deleteArtikel($id)
+	{
+		return $this->delete("artikelen", "artid=" . $id);
+	}
+	public function updateArtikel($id, $omschrijving, $inkoop, $verkoop, $voorraad, $minvoorraad, $maxvoorraad, $locatie, $levid)
+	{
+		return $this->update("artikelen", array("artikelenomschrijving" => $omschrijving, "artinkoop" => $inkoop, "artverkoop" => $verkoop, "artvoorraad" => $voorraad, "artminvoorraad" => $minvoorraad, "artmaxvoorraad" => $maxvoorraad, "artlocatie" => $locatie, "levid" => $levid), "levid=" . $id);
+	}
+
+}
