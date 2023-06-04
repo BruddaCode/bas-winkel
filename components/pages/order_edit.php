3cdc583b541823665644ab9a3b8887bf79a823e0
BruddaCode <Fabio.wolthuis@gmail.com>
2023-06-04 15:38:49 +0200

    updated order edit

--- components/pages/order_edit.php
+++ components/pages/order_edit.php
@@ -6,25 +6,17 @@ if (!isset($_GET["id"]) || strlen($_GET["id"]) == 0) {
 } else {
 
 	$order = new order();
-	$row = $order->selectOrders($_GET["id"])->fetch();
-
-	?>
+?>
 
 	<p class="lead display-4">Order Wijzigen</p>
 	<hr>
 	<form method="post" action="order_edit">
 		<div class="form-group">
-			<label>Select klant:</label>
-			<select class="form-control" name="klant">
+			<label>Klant naam:</label>
 				<?php
-				$orders = $order->selectKlanten();
-				while ($row = $orders->fetch()) {
-					$klantId = $row['klantid'];
-					$klantNaam = $row['klantnaam'];
-					echo "<option value=$klantId>$klantNaam (id: $klantId)</option>";
-				}
+					$row = $order->selectOrders($_GET["id"])->fetch();
+					echo "<p>{$row['klantnaam']}</p>";
 				?>
-			</select>
 		</div>
 
 		<div class="form-group">
