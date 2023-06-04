1c626ce4fe232cc4ff628b321d4adccbb5c5faa3
MVDW <maxvdwolf@gmail.com>
2023-06-04 16:29:52 +0200

    add leveranciers

--- backend/leverancier.php
+++ backend/leverancier.php
@@ -1,5 +1,33 @@
-<?php 
+<?php
+include_once 'crud.php';
+class leverancier extends crud
+{
 
-    class leverancier {
-        public $leverancierid;
-    }
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function insertLeverancier($id, $naam, $contact, $email, $adres, $postcode, $plaats)
+	{
+		return $this->insert("leveranciers", array("levid" => $id, "levnaam" => $naam, "levcontact" => $contact, "levemail" => $email, "levadres" => $adres, "levpostcode" => $postcode, "levplaats" => $plaats));
+	}
+	public function selectLeverancier($id = false)
+	{
+		if ($id) {
+			return $this->select("leveranciers", "*", "levid=" . $id);
+		} else {
+			return $this->select("leveranciers", "*");
+		}
+
+	}
+	public function deleteLeverancier($id)
+	{
+		return $this->delete("leveranciers", "levid=" . $id);
+	}
+	public function updateLeverancier($id, $naam, $contact, $email, $adres, $postcode, $plaats)
+	{
+		return $this->update("leveranciers", array("levid" => $id, "levnaam" => $naam, "levcontact" => $contact, "levemail" => $email, "levadres" => $adres, "levpostcode" => $postcode, "levplaats" => $plaats), "levid=" . $id);
+	}
+
+}
--- components/elements/navbar.php
+++ components/elements/navbar.php
@@ -18,6 +18,9 @@
 				<a class="nav-link" href="./home">Home</a>
 			</li>
 
+			<li class="nav-item <?php if ($page_manager->getRouteTitle() == "Inkoop") echo "active"; ?>">
+				<a class="nav-link" href=".\inkoop">Inkoop</a>
+			</li>
 
 			<li class="nav-item <?php if ($page_manager->getRouteTitle() == "Klanten") echo "active"; ?>">
 				<a class="nav-link" href=".\klanten">Klanten</a>
@@ -26,6 +29,10 @@
 
 			<li class="nav-item <?php if ($page_manager->getRouteTitle() == "Orders") echo "active"; ?>">
 				<a class="nav-link" href=".\orders">Orders</a>
+			</li>
+
+			<li class="nav-item <?php if ($page_manager->getRouteTitle() == "Leveranciers") echo "active"; ?>">
+				<a class="nav-link" href=".\leveranciers">Leveranciers</a>
 			</li>
 
 		</ul>
