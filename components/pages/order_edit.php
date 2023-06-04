1f09a2774871227fd7c35554a66118e2776be858
MVDW <maxvdwolf@gmail.com>
2023-06-04 13:51:11 +0200

    Update order_edit.php

--- components/pages/order_edit.php
+++ components/pages/order_edit.php
@@ -4,23 +4,39 @@ include_once("backend/order.php");
 if (!isset($_GET["id"]) || strlen($_GET["id"]) == 0) {
 	echo "Error: id has not been set.";
 } else {
+
 	$order = new order();
-	$row = $order->selectOrders($_GET["id"]);
-	$row = $row->fetch();
+	$row = $order->selectOrders($_GET["id"])->fetch();
+
 	?>
-	<h1>Edit Order</h1>
+
+	<p class="lead display-4">Order Wijzigen</p>
 	<hr>
 	<form method="post" action="order_edit">
-			<br>   
-		<label for="an">Artkel:</label>
-		<input type="text" id="" name="artikel" placeholder="artikel" value="<?php echo $row['artid']; ?>" required/>
-			<br>
-		<label for="an">Aantal:</label>
-		<input type="text" id="" name="aantal" placeholder="aantal" value="<?php echo $row['verkordbestaantal']; ?>" required/>
-			<br>
-		<label for="an">Status:</label>
-		<input type="text" id="" name="status" placeholder="status" value="<?php echo $row['verkordstatus']; ?>" required/>
-			<br><br>
+		<div class="form-group">
+			<label>Select klant:</label>
+			<select class="form-control" name="klant">
+				<?php
+				$orders = $order->selectKlanten();
+				while ($row = $orders->fetch()) {
+					$klantId = $row['klantid'];
+					$klantNaam = $row['klantnaam'];
+					echo "<option value=$klantId>$klantNaam (id: $klantId)</option>";
+				}
+				?>
+			</select>
+		</div>
+
+		<div class="form-group">
+			<label>Status:</label>
+			<select class="form-control" name="status">
+				<option value="0">In behandeling</option>
+				<option value="1">Verzonden</option>
+				<option value="2">Ontvangen</option>
+			</select>
+		</div>
+
+
 		<input type='submit' name='insert' value='Update'>
 	</form></br>
 
